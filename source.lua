a4c41c53bf0b355cff6c619f3d37fa5faf9c9bd0
CioKiTTY <rasyajusticio.exploit@gmail.com>
2024-12-25 20:05:04 +0700

    feat: add path finder for players and monsters

--- source.lua
+++ source.lua
@@ -31,23 +31,61 @@ local cfg = {}
 cfg.playerESPEnabled = false
 cfg.monstersESPEnabled = false
 
+cfg.loopFindPath = false
+cfg.maxComputeRetries = 3
+
 --<<->>-<<->>-<<->>-<<->>-<<->>-<<->>-<<->>-<<->>-<<->>-<<->>-<<->>-<<->>--
 ---<< Variables >---
 local maid = Maid.new()
+local pathTargetLists = {}
+local pathTarget = nil :: Model
 
 --<<->>-<<->>-<<->>-<<->>-<<->>-<<->>-<<->>-<<->>-<<->>-<<->>-<<->>-<<->>--
 ---<< Game Objects >>---
+local localPlayer = Players.LocalPlayer
 local npcs: Model = workspace:WaitForChild("NPCs")
+
 local highlighter: Folder = Instance.new("Folder")
+local waypointStorage: Model = Instance.new("Model")
 
 --<<->>-<<->>-<<->>-<<->>-<<->>-<<->>-<<->>-<<->>-<<->>-<<->>-<<->>-<<->>--
 ---<< Helper Functions >>---
+local function getHumanoidRootPart()
+	local character = localPlayer.Character
+
+	if not character then
+		return
+	end
+
+	return character:FindFirstChild("HumanoidRootPart")
+end
+
+local function getHumanoid()
+	local character = localPlayer.Character
+
+	if not character then
+		return
+	end
+
+	return character:FindFirstChildWhichIsA("Humanoid")
+end
+
+local function refreshPathTargetList()
+	local keys = {}
+	for key, _ in pairs(pathTargetLists) do
+		table.insert(keys, key)
+	end
+
+	UILibrary.Options["pathFindTarget"]:SetValues(keys)
+end
+
 local function isHighlighted(object: Instance)
 	return highlighter:FindFirstChild(object.Name)
 end
 
 --<<->>-<<->>-<<->>-<<->>-<<->>-<<->>-<<->>-<<->>-<<->>-<<->>-<<->>-<<->>--
 ---<< Core Functions >>---
+--<< ESP Functions >>--
 local function highlightMonsters()
 	if not isHighlighted(npcs) then
 		local highlight = Instance.new("Highlight")
@@ -100,6 +138,101 @@ local function unhighlightAllPlayers()
 	end
 end
 
+--<< Path Finding Functions >>--
+local function computePathTo(targetPosition: Vector3)
+	local humanoidRootPart = getHumanoidRootPart()
+
+	if not humanoidRootPart then
+		return
+	end
+
+	local path = PathfindingService:CreatePath()
+
+	local numOfRetries = 0
+	local success, errorMessage
+
+	repeat
+		numOfRetries += 1
+
+		success, errorMessage = pcall(path.ComputeAsync, path, humanoidRootPart.CFrame.Position, targetPosition)
+
+		if not success then
+			UILibrary:Notify({
+				Title = "1",
+				Content = "1",
+				Duration = 5,
+			})
+			task.wait(0.5)
+		end
+	until success == true or numOfRetries > cfg.maxComputeRetries
+
+	if success then
+		return path
+	else
+		UILibrary:Notify({
+			Title = "2",
+			Content = "2",
+			Duration = 5,
+		})
+		return
+	end
+end
+
+local function visualizePath(path: Path)
+	if path.Status == Enum.PathStatus.Success then
+		local waypoints = path:GetWaypoints()
+
+		for i, point in ipairs(waypoints) do
+			local marker = Instance.new("Part")
+			marker.Name = `Point {i}`
+
+			marker.Size = Vector3.new(0.8, 0.8, 0.8)
+			marker.CFrame = CFrame.new(point.Position)
+
+			marker.Color = Color3.fromRGB(0, 255, 0)
+			marker.Shape = Enum.PartType.Ball
+
+			marker.Anchored = true
+			marker.CanCollide = false
+			marker.CanQuery = false
+			marker.CanTouch = false
+
+			marker.Parent = waypointStorage
+		end
+	else
+		UILibrary:Notify({
+			Title = "3",
+			Content = "3",
+			Duration = 5,
+		})
+		return
+	end
+end
+
+local function loopFindPath()
+	if UILibrary.Unloaded then
+		return false
+	end
+
+	if not cfg.loopFindPath then
+		return true
+	end
+
+	if not pathTarget then
+		return true
+	end
+
+	local path = computePathTo(pathTarget:GetPivot().Position)
+	if not path then
+		return true
+	end
+
+	waypointStorage:ClearAllChildren()
+	visualizePath(path)
+
+	return true
+end
+
 --<<->>-<<->>-<<->>-<<->>-<<->>-<<->>-<<->>-<<->>-<<->>-<<->>-<<->>-<<->>--
 ---<< Interface >>---
 local Window = UILibrary:CreateWindow({
@@ -116,6 +249,7 @@ local Window = UILibrary:CreateWindow({
 
 local Tabs = {
 	ESP = Window:CreateTab({ Title = "ESP", Icon = "radar" }),
+	PathFinder = Window:CreateTab({ Title = "Path Finder", Icon = "route" }),
 	Settings = Window:CreateTab({ Title = "Settings", Icon = "settings" }),
 }
 
@@ -147,6 +281,51 @@ do
 	end
 end
 
+--< PathFinder
+do
+	local tab = Tabs.PathFinder
+
+	tab:CreateDropdown("pathFindTarget", {
+		Title = "Target",
+		Values = {"None"},
+		Multi = false,
+		Default = 1
+	})
+
+	tab:CreateToggle("loopFindPath", {
+		Title = "Find Path Continuously",
+		Default = cfg.loopFindPath,
+	})
+
+	tab:CreateButton({
+		Title = "Find Path",
+		Description = "Find the path to the target",
+		Callback = function()
+			if not pathTarget then
+				UILibrary:Notify({
+					Title = "No target",
+					Content = "Please select a target",
+					Duration = 5,
+				})
+				return
+			end
+
+			local path = computePathTo(pathTarget:GetPivot().Position)
+			if not path then
+				UILibrary:Notify({
+					Title = "69",
+					Content = "69",
+					Duration = 5,
+				})
+				return
+			end
+
+			waypointStorage:ClearAllChildren()
+			visualizePath(path)
+		end,
+	})
+end
+
 --<< Logic >>--
 --< ESP
 do
@@ -167,6 +346,19 @@ do
 			highlightMonsters()
 		else
 			unhighlightMonsters()
+		end
+	end)
+end
+
+--< PathFinder
+do
+	Elements["loopFindPath"]:OnChanged(function()
+		cfg.loopFindPath = Elements["loopFindPath"].Value
+	end)
+
+	Elements["pathFindTarget"]:OnChanged(function(value)
+		if pathTargetLists[value] and pathTargetLists[value].Parent ~= nil then
+			pathTarget = pathTargetLists[value]
 		end
 	end)
 end
@@ -202,17 +394,49 @@ end
 highlighter.Name = "Highlighter"
 highlighter.Parent = CoreGui
 
+if workspace:FindFirstChild("Waypoints") then
+	workspace:FindFirstChild("Waypoints"):Destroy()
+end
+
+local waypointsHighlight = Instance.new("Highlight")
+waypointsHighlight.FillColor = Color3.fromRGB(0, 255, 0)
+waypointsHighlight.Adornee = waypointStorage
+waypointsHighlight.Parent = highlighter
+
+waypointStorage.Name = "Waypoints"
+waypointStorage.Parent = workspace
+
 --<<->>-<<->>-<<->>-<<->>-<<->>-<<->>-<<->>-<<->>-<<->>-<<->>-<<->>-<<->>--
 ---<< Main >>---
---< ESP
 highlightMonsters()
+
+for _, npc in (npcs:GetChildren()) do
+	pathTargetLists[npc.Name] = npc
+	refreshPathTargetList()
+end
 
 maid:GiveTask(Players.PlayerAdded:Connect(function(player)
 	maid:GiveTask(player.CharacterAdded:Connect(function(character)
 		if cfg.playerESPEnabled then
 			highlightPlayer(character)
 		end
+
+		pathTargetLists[player.Name] = character
+		refreshPathTargetList()
+
+		local humanoid = character:WaitForChild("Humanoid")
+		maid:GiveTask(humanoid.Died:Connect(function()
+			pathTargetLists[player.Name] = nil
+			refreshPathTargetList()
+		end))
 	end))
+end))
+
+maid:GiveTask(Players.PlayerRemoving:Connect(function(player)
+	if pathTargetLists[player.Name] then
+		pathTargetLists[player.Name] = nil
+		refreshPathTargetList()
+	end
 end))
 
 for _, player in ipairs(Players:GetPlayers()) do
@@ -223,9 +447,33 @@ for _, player in ipairs(Players:GetPlayers()) do
 	if cfg.playerESPEnabled then
 		highlightPlayer(player.Character)
 	end
+	pathTargetLists[player.Name] = player.Character
+	refreshPathTargetList()
+	
 	maid:GiveTask(player.CharacterAdded:Connect(function(character)
 		if cfg.playerESPEnabled then
 			highlightPlayer(character)
 		end
+
+		pathTargetLists[player.Name] = character
+		refreshPathTargetList()
+		
+		local humanoid = character:WaitForChild("Humanoid")
+		maid:GiveTask(humanoid.Died:Connect(function()
+			pathTargetLists[player.Name] = nil
+			refreshPathTargetList()
+		end))
 	end))
 end
+
+task.spawn(function()
+	while true do
+		task.wait()
+
+		local canContinue = loopFindPath()
+
+		if not canContinue then
+			break
+		end
+	end
+end)
